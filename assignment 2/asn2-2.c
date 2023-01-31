7c116852648bfffbf8593a9c7db6090c0b6b8ce9
cwhaley112 <cwhaley112@gmail.com>
2023-01-30 20:02:04 -0500

    assignment 2: refactored from char** array to corpus struct

--- assignment 2/asn2-2.c
+++ assignment 2/asn2-2.c
@@ -5,46 +5,51 @@
 // #include "./c-vector/vec.h"
 // #include "./uthash/src/uthash.h"
 
+struct corpus {
+	char** words;
+	int len;
+};
+
 #define maxlen 50
 #define maxwords 5000000
+typedef struct corpus corpus;
 
-int main() {
-	// wikitext dataset: C:\Users\cwhal\coding\random_notebooks\datasets\wikitext.txt
-
+corpus* read_corpus() {
 	FILE* f;
 	char buf[maxlen];
 	char c;
-	int i,start,numwords;
+	int i,start;
+	corpus* cor = malloc(sizeof(corpus));
 
 	f = fopen("/mnt/c/Users/cwhal/coding/random_notebooks/datasets/wikitext.txt", "r");
-	// f = fopen("./test.txt", "r");
 	if (f == NULL) {
 		printf("Couldn't open file\n");
-		return 0;
+		return cor;
 	}
 
-	i=0; start=0; numwords=0;
-	char** words = malloc(sizeof(char*)*maxwords);
+	i=0; start=0;
+	cor->len = 0;
+	cor->words = malloc(sizeof(char*)*maxwords);
 	do {
 		// using fgetc:
 		c = fgetc(f);
 		if (c==EOF) {
 			//add final word
 			if (i-start>0) {
-				words[numwords] = malloc(sizeof(char)*(i-start)+1); // create string to store new word
-				strncpy(words[numwords], buf, (i-start)); // copy string from buffer to heap
-				words[numwords][i-start] = '\0';
-				numwords++;
+				cor->words[cor->len] = malloc(sizeof(char)*(i-start)+1); // create string to store new word
+				strncpy(cor->words[cor->len], buf, (i-start)); // copy string from buffer to heap
+				cor->words[cor->len][i-start] = '\0';
+				cor->len++;
 			} 
 			break;
 		}
 
 		if (!isalpha(c) && (i-start)>0){
 			// add word to array
-			words[numwords] = malloc(sizeof(char)*(i-start)+1); // create string to store new word
-			strncpy(words[numwords], buf, (i-start)); // copy string from buffer to heap
-			words[numwords][i-start] = '\0';
-			numwords++;
+			cor->words[cor->len] = malloc(sizeof(char)*(i-start)+1); // create string to store new word
+			strncpy(cor->words[cor->len], buf, (i-start)); // copy string from buffer to heap
+			cor->words[cor->len][i-start] = '\0';
+			cor->len++;
 			start = i+1; // move start
 		}
 		else if (isalpha(c)) {
@@ -53,20 +58,25 @@ int main() {
 		}
 		else start=i+1; //i-start==1; move start to the right so word doesn't start with whitespace
 
-		if (numwords==maxwords) break;
+		if (cor->len==maxwords) break;
 		i++;
 	} while (1);
 
-	// print each word, then free memory from heap:
-	for (i=0; i<numwords; i++) {
+	fclose(f);
+	return cor;
+}
+
+int main() {
+	int i;
+	corpus* cor = read_corpus();
+
+
+	// free memory from heap:
+	for (i=0; i<cor->len; i++) {
 		// printf("%s\n", words[i]);
-		free(words[i]);
+		free(cor->words[i]);
 	}
 
-	free(words);
-
-	// strncpy(buf, f, 15);
-
-	fclose(f);
+	free(cor);
 	return 0;
 }
