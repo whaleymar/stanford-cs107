f7b01dcd5665010d557e34616c05d3fdf9721193
cwhaley112 <cwhaley112@gmail.com>
2023-02-05 22:16:19 -0500

    assignment 3: some reorganizing, abracting linked list search next

--- assignment 3/cmap.c
+++ assignment 3/cmap.c
@@ -4,71 +4,17 @@
 #include <string.h>
 #include "cmap.h"
 
-//string hashing algorithm from http://www.cse.yorku.ca/~oz/hash.html
-static unsigned long hash(const char *str, const unsigned int mod) {
-    unsigned long hash = 5381;
-    int c;
+//helper functions:
+static unsigned long hash(const char *str, const unsigned int mod);
+static void **get_bucket(const map *h, int ix);
+static char *keyptr(void **blob);
+static void *valptr(void **blob);
+static void set_val(void **blob, const void *valAddr, size_t valsz);
+static void **new_blob(const char *key, size_t valsz);
+static void **next_blob(void **ptr);
 
-    while (c = *str++)
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-
-    return hash % mod;
-}
-
-// static void *_cmap_get(const map *h, int ix)
-
-static void **get_bucket(const map *h, int ix) {
-	//helper for getting address of buckets[ix]
-	return (void **)((char *)h->buckets + ix*sizeof(char *));
-}
-
-static char *keyptr(void **blob) {
-	//gives starting address of key within blob
-	return (char *)blob+sizeof(char*);
-}
-
-static void *valptr(void **blob) {
-	//gives starting address of value within blob
-
-	//find key
-	char *ptr = keyptr(blob);
-	//find string terminator
-	ptr = strchr(ptr, '\0');
-	//make sure it was found
-	assert(ptr!=NULL);
-
-	return ptr+1;
-}
-
-static void set_val(void **blob, const void *valAddr, size_t valsz) {
-	void *vptr = valptr(blob);
-	memcpy(vptr, valAddr, valsz);
-}
-
-static void **new_blob(const char *key, size_t valsz) {
-	//allocate memory to store:
-	//	1) pointer to next element (new blob is always last element so this is set to NULL)
-	//	2) char buffer for key string
-	//	3) space for value (will be set by another helper fxn)
-	// printf("new blob\n");
-
-	void **blob = malloc(sizeof(char *) + (strlen(key)+1) + valsz);
-
-	//set first 8 bytes to null pointer
-	*blob = NULL;
-
-	//after pointer, add key string
-	strcpy(keyptr(blob), key);
-
-	return blob;
-}
-
-static void **next_blob(void **ptr) {
-	return *ptr;
-}
-
-void cmap_new(map *h, int elemSize, int numBuckets,
-	HashMapFreeFunction freefn) {
+//client functions:
+void cmap_new(map *h, int elemSize, int numBuckets, HashMapFreeFunction freefn) {
 
  	assert(elemSize>0 && numBuckets>0);
 
@@ -103,10 +49,8 @@ void *cmap_get(const map *h, const char *key) {
 	int ix = hash(key, h->nbuckets);
 	void **ptr = *get_bucket(h, ix);
 	
-	if (ptr==NULL) return NULL;
-	//go through linkedlist looking for key, return value
-	else {
-		// go through linkedlist looking for key,
+	if (ptr!=NULL) {
+		// go through linkedlist looking for key, return value
 		do
 		 {
 		 	if (strcmp(key, keyptr(ptr))==0) {
@@ -122,33 +66,85 @@ void *cmap_get(const map *h, const char *key) {
 void cmap_put(map *h, const char *key, const void *valAddr) {
 	int ix = hash(key, h->nbuckets);
 	void **ptr = get_bucket(h, ix);
-	void **ptrog = ptr;
+	char found=0;
 
-	if (*ptr==NULL) {
-		//bucket is empty, create new blob and point bucket to it
-		*ptr = new_blob(key, h->valsz);
-		ptr = next_blob(ptr);
-	}
-	else {
+	if (*ptr!=NULL) {
 		// go through linkedlist looking for key,
-		char found=0;
 		ptr = next_blob(ptr);
-		do
-		 {
+		do {
 		 	if (strcmp(key, keyptr(ptr))==0) {
 		 		found=1;
 		 		break;
 		 	}
 		 	if (*ptr==NULL) break;
 		 	ptr = next_blob(ptr);
-		 } while (1);
-		 if (!found) {
-		 	*ptr = new_blob(key, h->valsz);
-		 	ptr = next_blob(ptr);
-		 }
+		} while (1);
+	}
+
+	if (!found) {
+		*ptr = new_blob(key, h->valsz);
+		ptr = next_blob(ptr);
 	}
 
 	set_val(ptr, valAddr, h->valsz);
 }
 
 //TODO; cmap_remove, cmap_first/cmap_next (for client iteration)
+void cmap_remove(map *h, const char *key) {
+
+
+}
+
+////////////////////////////////
+//helper function definitions://
+////////////////////////////////
+
+//string hashing algorithm from http://www.cse.yorku.ca/~oz/hash.html
+static unsigned long hash(const char *str, const unsigned int mod) {
+    unsigned long hash = 5381;
+    int c;
+    while (c = *str++)
+        hash = ((hash << 5) + hash) + c;
+    return hash % mod;
+}
+
+//helper for getting address of buckets[ix]
+static void **get_bucket(const map *h, int ix) {
+	return (void **)((char *)h->buckets + ix*sizeof(char *));
+}
+
+//gives starting address of key within block of memory
+static char *keyptr(void **blob) {
+	return (char *)blob+sizeof(char*);
+}
+
+//gives starting address of value within blob
+static void *valptr(void **blob) {
+	char *ptr = keyptr(blob); //find key
+	ptr = strchr(ptr, '\0'); //find string terminator
+	assert(ptr!=NULL); //make sure it was found
+
+	return ptr+1;
+}
+
+// sets value part of key:value pair within mem block
+static void set_val(void **blob, const void *valAddr, size_t valsz) {
+	void *vptr = valptr(blob);
+	memcpy(vptr, valAddr, valsz);
+}
+
+//allocate a block (or "blob" :^) ) of memory to store:
+	//	1) pointer to next element (this is set to NULL because a new blob will always be last in the linkedlist)
+	//	2) char buffer for key string
+	//	3) space for value (will be set by another helper fxn)
+static void **new_blob(const char *key, size_t valsz) {
+	void **blob = malloc(sizeof(char *) + (strlen(key)+1) + valsz);
+	*blob = NULL; //set first 8 bytes to null pointer
+	strcpy(keyptr(blob), key);//after pointer, add key string
+	return blob;
+}
+
+//given a blob, gets next one in linked list
+static void **next_blob(void **ptr) {
+	return *ptr;
+}
